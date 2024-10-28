cdfcb6d55f483455f201840f933569692d1dfd2f
Luca2700 <lucarodenas7@gmail.com>
2024-10-28 15:38:13 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -6,18 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main className="App-main">
+        <div>Titulo</div>
+        <input type='button' value="+" className="App-button"></input>
+      </main>
     </div>
   );
 }
